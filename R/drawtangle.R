5355b3411d3cdb4500f1d0e704ce94cc66cd0a3d
16xchen <16xchen@mdirss.org>
2015-11-30 22:38:49 -0500

    add description, modified functions

--- R/drawtangle.R
+++ R/drawtangle.R
@@ -4,7 +4,7 @@
 #'
 #' @param snptree A list of dendrograms generated from snp data
 #' @param trait.dend A dendrogram generated from quantitative trait data
-#' @param cordata A dataframe which includes positions and corrlation values of highly correlated snp trees
+#' @param indata A dataframe similar to outputs by {snocor} or {snpcor.best}, that determines which SNP trees to plot out as tanglegrams.
 #' @param chr.num The chromosome number from which the snps are located
 #' @return Plots highlt correlated tanglegrams with SNP location on chromosome in basepairs
 #' @examples
@@ -24,25 +24,25 @@
 #'
 #' mycordata=snpcor(snpdata=chrX, snptree=mysnptree, trait.dend=mytrait.dend) #calculate Baker's correlation coefficient for trait dendrogram and each SNP dendrogram
 #' mycordata.best=snpcor.best(cordata=mycordata,threshold = 0.95) #find highly correlated SNP dendrograms
-#' drawtangle(snptree=mysnptree, trait.dend=mytrait.dend, cordata.best=mycordata.best, chr.num="X") #plot out as tanglegrams
+#' drawtangle(snptree=mysnptree, trait.dend=mytrait.dend, indata=mycordata.best, chr.num="X") #plot out as tanglegrams
 #'
 #' @author Xingyao Chen
 #' @export
 #'
 #'
-drawtangle = function(snptree, trait.dend, cordata.best, chr.num)
+drawtangle = function(snptree, trait.dend, indata, chr.num)
 {
   require("dendextend") || install.packages('dendextend')
   library(dendextend)
-  if(ncol(cordata.best)==1){
-    cordata.best=t(cordata.best)
+  if(ncol(indata)==1){
+    indata=t(indata)
   }
-  for(j in 1:nrow(cordata.best)){
-  tanglegram(untangle(intersect_trees(trait.dend, snptree[[cordata.best[j,1]]])),
+  for(j in 1:nrow(indata)){
+  tanglegram(untangle(intersect_trees(trait.dend, snptree[[indata[j,1]]])),
              main="Correlations",cex_main=1,
              main_left = "traitbiome tree",
-             main_right= paste("chr",chr.num, "SNP @", cordata.best[j,2],"to", cordata.best[j,3], "bp", sep=" "),
-             sub=paste("correlation",round(cordata.best[j, 4],digit=4),
+             main_right= paste("chr",chr.num, "SNP @", indata[j,2],"to", indata[j,3], "bp", sep=" "),
+             sub=paste("correlation",round(indata[j, 4],digit=4),
                        sep=":"), cex_sub=0.9, highlight_distinct_edges=F,
              lwd=1.5, columns_width=c(4.5,1.8,4.5),
              margin_outer = 3,
