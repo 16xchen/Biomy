72764ffb8c9fb5865e941c03f3f53e2c7ccf51e1
16xchen <16xchen@mdirss.org>
2015-11-30 20:36:58 -0500

    new use for old package

--- ExampleScript/ExampleScript.R
+++ ExampleScript/ExampleScript.R
@@ -1,5 +1,5 @@
 #Example script using package Biomy
-#Correlating relative abunadnce microbiome of host strain to Single Nucleotide Polymorphisms of host strain
+#Correlating relative trait microbiome of host strain to Single Nucleotide Polymorphisms of host strain
 #author: Xingyao Chen
 #date: 11/29/15
 #
@@ -13,14 +13,14 @@ chrX = SampSNP()
 mysnptree=maketree(chrX, strain.names=c(paste("strain", 1:(ncol(chrX)-1), sep="")))
 #
 set.seed(1234)
-x <- rnorm(n=120, mean=5, sd=20)
-abundance=matrix(x,10,12)
-rname=paste("bacteria",1:10,sep="")
+x <- rnorm(n=(16*12), mean=10, sd=10)
+trait=matrix(x,16,12)
+rname=paste("trait",1:16,sep="")
 cname=paste("strain", 1:12, sep="")
-rownames(abundance)=rname
-colnames(abundance)=cname
+rownames(trait)=rname
+colnames(trait)=cname
 #
-mymicro.dend=microtree(abundance, nboot=10)
+mymicro.dend=microtree(trait, nboot=10)
 mycordata=snpcor(snpdata=chrX, snptree=mysnptree, micro.dend=mymicro.dend)
 mycordata.best=snpcor.best(cordata=mycordata,threshold = 0.9)
 drawtangle(snptree=mysnptree, micro.dend=mymicro.dend, cordata.best=mycordata.best, chr.num="X")
--- R/traittree.R
+++ R/traittree.R
@@ -1,23 +1,23 @@
-#' Clustering strains base on microbiome relative abundance data
+#' Clustering strains base on microbiome relative trait data
 #'
 #' Calculates p-values for hierarchical clustering via multiscale bootstrap resampling.
 #'
 #' Hierarchical clustering is done for given data and p-values are computed for each of the clusters.
 #'
 #'
-#' @param microdata A matrix of relative abundance data
+#' @param microdata A matrix of relative trait data
 #' @param nboot The number of bootstrap replications. The default is 1000.
 #' @return An object of dendrgram class
 #' @examples
-#' set.seed(1234)  #simulate microbiome relative abundance data
-#' x <- rnorm(n=120, mean=5, sd=20)
-#' abundance=matrix(x,10,12)
-#' rname=paste("bacteria",1:10,sep="")
-#' cname=paste("species", 1:12, sep="")
-#' rownames(abundance)=rname
-#' colnames(abundance)=cname
-#' #
-#' microdend=microtree(microdata=abundance, nboot=10)
+#' set.seed(1234)  #simulate microbiome relative trait data
+#' x <- rnorm(n=(16*12), mean=10, sd=10)
+#' trait=matrix(x,16,12)
+#' rname=paste("trait",1:16,sep="")
+#' cname=paste("strain", 1:12, sep="")
+#' rownames(trait)=rname
+#' colnames(trait)=cname
+#'
+#' microdend=microtree(microdata=trait, nboot=10)
 #' microdend
 #' plot(microdend)
 #'
@@ -26,11 +26,11 @@
 #'
 #'
 #'
-microtree=function(mircodata, nboot=1000){
+traittree=function(mircodata, nboot=1000){
   require("pvclust")  || install.packages("pvclust")
   library("pvclust")
   require("dendextend") || install.packages('dendextend')
   library(dendextend)
-  a.clust = pvclust(abundance, method.dist="cor", method.hclust="complete", nboot=nboot)
+  a.clust = pvclust(trait, method.dist="cor", method.hclust="complete", nboot=nboot)
   return(as.dendrogram(a.clust))
 }
